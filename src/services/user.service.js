4842041d66e263ff36386aba0ecd0756a07a0f16
vish <vishgirase95@gmail.com>
2022-01-24 22:06:03 +0530

    [vishal] added login

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,7 +1,12 @@
 import User from '../models/user.model';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config('../.env');
 
 
+
+const LOGIN_TOKEN = process.env.TOKEN;
 
 //create new user 
 export const newUser = async (body) => {
@@ -11,13 +16,43 @@ export const newUser = async (body) => {
   if (checkUser) {
     throw Error("User Already Exsist")
   } else {
-    const data = await User.create(body);
-    const unHashedPassword=data.Password;
+    const unHashedPassword = body.Password;
     const saltRounds = 10;
-    const HashedPassword=await bcrypt.hash(unHashedPassword,saltRounds)
-    data.Password=HashedPassword;
+    const HashedPassword = await bcrypt.hash(unHashedPassword, saltRounds)
+    body.Password = HashedPassword;
+    const data = await User.create(body);
+
     return data;
   }
 
-
 };
+
+
+
+// login user
+export const login = async (body) => {
+  const mailVerify = await User.findOne({
+    Email: body.Email
+  })
+  if (mailVerify) {
+    const HashedPassword = mailVerify.Password;
+    const EnterPassword = body.Password
+    const isMatch = await bcrypt.compare(EnterPassword, HashedPassword);
+    if (isMatch) {
+
+      const token = jwt.sign({
+        Email: mailVerify.Email,
+        ID: mailVerify._id
+      }, LOGIN_TOKEN)
+
+
+      return token;
+    } else {
+      throw Error("Please enter corret mail id or password");
+    }
+
+  } else {
+    throw Error("Please enter correct Mail Id");
+  }
+
+}
