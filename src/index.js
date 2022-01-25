6e458e909432db62b38f477e9d0936a942a0b886
vish <vishgirase95@gmail.com>
2022-01-26 00:26:46 +0530

    [vishal] added reg swagger

--- src/index.js
+++ src/index.js
@@ -15,11 +15,17 @@ import {
 import logger, { logStream } from './config/logger';
 
 import morgan from 'morgan';
-
+// import swaggerJSDoc from 'swagger-jsdoc';
+const swaggerUi = require('swagger-ui-express');
+import swaggerOptions from './swagger/swagger.json'
 const app = express();
 const host = process.env.APP_HOST;
 const port = process.env.APP_PORT;
 const api_version = process.env.API_VERSION;
+
+
+
+app.use('/swagger_doc', swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 
 app.use(cors());
 app.use(helmet());
